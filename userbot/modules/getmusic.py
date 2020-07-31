38c5bd43db2e6b8de03957f71910986f38647fda
Frizzy | ‎미나 <dhanikata@gmail.com>
2020-07-31 17:22:18 +0700

    getmusic: improvement and integrate lastfm scrobbles

--- userbot/modules/getmusic.py
+++ userbot/modules/getmusic.py
@@ -3,45 +3,23 @@ import glob
 import os
 import subprocess
 import time
+from asyncio.exceptions import TimeoutError
 
 import requests
 from bs4 import BeautifulSoup
 from hachoir.metadata import extractMetadata
 from hachoir.parser import createParser
+from pylast import User
 from telethon import events
 from telethon.errors.rpcerrorlist import YouBlockedUserError
 from telethon.tl.types import DocumentAttributeVideo
 
-from userbot import CMD_HELP, bot
+from userbot import CMD_HELP, LASTFM_USERNAME, bot, lastfm
 from userbot.events import register
 from userbot.utils import progress
 
 
 # For song module
-def get_readable_time(seconds: int) -> str:
-    count = 0
-    up_time = ""
-    time_list = []
-    time_suffix_list = ["s", "m", "h", "days"]
-    while count < 4:
-        count += 1
-        remainder, result = divmod(
-            seconds, 60) if count < 3 else divmod(
-            seconds, 24)
-        if seconds == 0 and remainder == 0:
-            break
-        time_list.append(int(result))
-        seconds = int(remainder)
-    for x in range(len(time_list)):
-        time_list[x] = str(time_list[x]) + time_suffix_list[x]
-    if len(time_list) == 4:
-        up_time += time_list.pop() + ", "
-    time_list.reverse()
-    up_time += ":".join(time_list)
-
-    return up_time
-
-
 def getmusic(get, DEFAULT_AUDIO_QUALITY):
     search = get
 
@@ -202,87 +180,122 @@ async def _(event):
     os.system("rm -rf *.webm")
 
 
-@register(outgoing=True, pattern=r"^\.smd(?: |$)(.*)")
+@register(outgoing=True, pattern=r"^\.smd (?:(now)|(.*) - (.*))")
 async def _(event):
     if event.fwd_from:
         return
-    link = event.pattern_match.group(1)
+    if event.pattern_match.group(1) == "now":
+        playing = User(LASTFM_USERNAME, lastfm).get_now_playing()
+        if playing is None:
+            return await event.edit("`Error: No scrobbling data found.`")
+        artist = playing.get_artist()
+        song = playing.get_title()
+    else:
+        artist = event.pattern_match.group(2)
+        song = event.pattern_match.group(3)
+    track = str(artist) + " - " + str(song)
     chat = "@SpotifyMusicDownloaderBot"
-    await event.edit("```Getting Your Music```")
-    async with bot.conversation(chat) as conv:
-        await asyncio.sleep(2)
-        await event.edit("`Downloading music taking some times,  Stay Tuned.....`")
-        try:
-            response = conv.wait_event(
-                events.NewMessage(incoming=True, from_users=752979930)
-            )
-            await bot.send_message(chat, link)
-            respond = await response
-            await bot.send_read_acknowledge(conv.chat_id)
-        except YouBlockedUserError:
-            await event.reply(
-                "```Please unblock @SpotifyMusicDownloaderBot and try again```"
-            )
-            return
+    try:
+        await event.edit("`Getting Your Music`")
+        async with bot.conversation(chat) as conv:
+            await asyncio.sleep(2)
+            await event.edit("`Downloading...`")
+            try:
+                response = conv.wait_event(
+                    events.NewMessage(incoming=True, from_users=752979930)
+                )
+                msg = await bot.send_message(chat, track)
+                respond = await response
+                res = conv.wait_event(
+                    events.NewMessage(incoming=True, from_users=752979930)
+                )
+                r = await res
+                await bot.send_read_acknowledge(conv.chat_id)
+            except YouBlockedUserError:
+                await event.reply("`Unblock `@SpotifyMusicDownloaderBot` and retry`")
+                return
+            await bot.forward_messages(event.chat_id, respond.message)
+        await event.client.delete_messages(conv.chat_id, [msg.id, r.id, respond.id])
         await event.delete()
-        await bot.forward_messages(event.chat_id, respond.message)
-        await bot.send_read_acknowledge(event.chat_id)
+    except TimeoutError:
+        return await event.edit(
+            "`Error: `@SpotifyMusicDownloaderBot` is not responding or Song not found!.`"
+        )
 
 
-@register(outgoing=True, pattern=r"^\.net(?: |$)(.*)")
+@register(outgoing=True, pattern=r"^\.net (?:(now)|(.*) - (.*))")
 async def _(event):
     if event.fwd_from:
         return
-    song = event.pattern_match.group(1)
+    if event.pattern_match.group(1) == "now":
+        playing = User(LASTFM_USERNAME, lastfm).get_now_playing()
+        if playing is None:
+            return await event.edit("`Error: No current scrobble found.`")
+        artist = playing.get_artist()
+        song = playing.get_title()
+    else:
+        artist = event.pattern_match.group(2)
+        song = event.pattern_match.group(3)
+    track = str(artist) + " - " + str(song)
     chat = "@WooMaiBot"
-    link = f"/netease {song}"
-    await event.edit("```Getting Your Music```")
-    async with bot.conversation(chat) as conv:
-        await asyncio.sleep(2)
-        await event.edit("`Downloading...Please wait`")
-        try:
-            msg = await conv.send_message(link)
-            response = await conv.get_response()
-            respond = await conv.get_response()
-            """ - don't spam notif - """
-            await bot.send_read_acknowledge(conv.chat_id)
-        except YouBlockedUserError:
-            await event.reply("```Please unblock @WooMaiBot and try again```")
-            return
-        await event.edit("`Sending Your Music...`")
-        await asyncio.sleep(3)
-        await bot.send_file(event.chat_id, respond)
-    await event.client.delete_messages(conv.chat_id, [msg.id, response.id, respond.id])
-    await event.delete()
+    link = f"/netease {track}"
+    await event.edit("`Searching...`")
+    try:
+        async with bot.conversation(chat) as conv:
+            await asyncio.sleep(2)
+            await event.edit("`Processing... Please wait`")
+            try:
+                msg = await conv.send_message(link)
+                response = await conv.get_response()
+                respond = await conv.get_response()
+                await bot.send_read_acknowledge(conv.chat_id)
+            except YouBlockedUserError:
+                await event.reply("`Please unblock @WooMaiBot and try again`")
+                return
+            await event.edit("`Sending Your Music...`")
+            await asyncio.sleep(3)
+            await bot.send_file(event.chat_id, respond)
+        await event.client.delete_messages(
+            conv.chat_id, [msg.id, response.id, respond.id]
+        )
+        await event.delete()
+    except TimeoutError:
+        return await event.edit(
+            "`Error: `@WooMaiBot` is not responding or Song not found!.`"
+        )
 
 
-@register(outgoing=True, disable_errors=True, pattern=r"^\.sdd(?: |$)(.*)")
+@register(outgoing=True, pattern=r"^\.sdd(?: |$)(.*)")
 async def _(event):
     if event.fwd_from:
         return
     d_link = event.pattern_match.group(1)
     if ".com" not in d_link:
-        await event.edit("` I need a link to download something pro.`**(._.)**")
+        await event.edit("`Enter a valid link to download from`")
     else:
-        await event.edit("**Initiating Download!**")
+        await event.edit("`Processing...`")
     chat = "@MusicHuntersBot"
-    async with bot.conversation(chat) as conv:
-        try:
-            msg_start = await conv.send_message("/start")
-            response = await conv.get_response()
-            msg = await conv.send_message(d_link)
-            details = await conv.get_response()
-            song = await conv.get_response()
-            """ - don't spam notif - """
-            await bot.send_read_acknowledge(conv.chat_id)
-        except YouBlockedUserError:
-            await event.edit("**Error:** `unblock` @MusicHuntersBot `and retry!`")
-            return
-        await bot.send_file(event.chat_id, song, caption=details.text)
-        await event.client.delete_messages(
-            conv.chat_id, [msg_start.id, response.id, msg.id, details.id, song.id]
+    try:
+        async with bot.conversation(chat) as conv:
+            try:
+                msg_start = await conv.send_message("/start")
+                response = await conv.get_response()
+                msg = await conv.send_message(d_link)
+                details = await conv.get_response()
+                song = await conv.get_response()
+                await bot.send_read_acknowledge(conv.chat_id)
+            except YouBlockedUserError:
+                await event.edit("`Unblock `@MusicHuntersBot` and retry`")
+                return
+            await bot.send_file(event.chat_id, song, caption=details.text)
+            await event.client.delete_messages(
+                conv.chat_id, [msg_start.id, response.id, msg.id, details.id, song.id]
+            )
+            await event.delete()
+    except TimeoutError:
+        return await event.edit(
+            "`Error: `@MusicHuntersBot` is not responding or Song not found!.`"
         )
-        await event.delete()
 
 
 CMD_HELP.update(
@@ -292,10 +305,12 @@ CMD_HELP.update(
         ".vsong **Artist - Song Title**"
         "\nUsage: Finding and uploading videoclip.\n\n"
         ".smd **Artist - Song Title**"
-        "\nUsage: Download music from spotify.\n\n"
-        ".net **Artist - Song Title**"
-        "\nUsage: Download music with @WooMaiBot.\n\n"
-        ".sdd <Spotify/Deezer Link>"
-        "\nUsage: Download music from Spotify or Deezer."
-    }
-)
+        "\nUsage: Download music from spotify use @SpotifyMusicDownloaderBot.\n\n"
+        ".smd now"
+        "\nUsage: Download current LastFM scrobble use @SpotifyMusicDownloaderBot.\n\n"
+        ">.net **Artist - Song Title**"
+        "\nUsage: Download music use @WooMaiBot.\n\n"
+        ">.net now"
+        "\nUsage: Download current LastFM scrobble use @WooMaiBot.\n\n"
+        ">.sdd <Spotify/Deezer Link>"
+        "\nUsage: Download music from Spotify or Deezer use @MusicHuntersBot."})
