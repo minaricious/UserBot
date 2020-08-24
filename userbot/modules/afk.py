181837cdc1152eb7e51fdb7a0eedb8cbd31d2b58
Frizzy | ‎미나 <dhanikata@gmail.com>
2020-08-25 01:58:29 +0700

    afk: disable autoflake linting on import

--- userbot/modules/afk.py
+++ userbot/modules/afk.py
@@ -11,7 +11,16 @@ from random import choice, randint
 
 from telethon.events import StopPropagation
 
-from userbot import (AFKREASON, CMD_HELP, BOTLOG, BOTLOG_CHATID, PM_AUTO_BAN)
+from userbot import (
+    AFKREASON,
+    COUNT_MSG,
+    CMD_HELP,
+    ISAFK,
+    BOTLOG,
+    BOTLOG_CHATID,
+    USERS,
+    PM_AUTO_BAN)  # pylint: disable=unused-imports
+
 from userbot.events import register
 
 # ========================= CONSTANTS ============================
