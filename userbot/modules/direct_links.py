17b9ca9346025c5f629fa4f15a0c3c29b7ddf01a
Frizzy | ‎미나 <dhanikata@gmail.com>
2020-09-02 03:19:55 +0700

    direct_links: add uptobox from ProjectBish repo

--- userbot/modules/direct_links.py
+++ userbot/modules/direct_links.py
@@ -5,32 +5,37 @@
 #
 """ Userbot module containing various sites direct links generators"""
 
-from subprocess import PIPE, Popen
+from asyncio import create_subprocess_shell as asyncSubprocess
+from asyncio.subprocess import PIPE as asyncPIPE
+
+import asyncio
 import re
 import urllib.parse
 import json
+import aiohttp
 from random import choice
 import requests
 from bs4 import BeautifulSoup
 from humanize import naturalsize
 
-from userbot import CMD_HELP
+from userbot import CMD_HELP, USR_TOKEN
+from userbot.utils import time_formatter
 from userbot.events import register
 
 
-def subprocess_run(cmd):
+async def subprocess_run(cmd):
     reply = ""
-    subproc = Popen(cmd, stdout=PIPE, stderr=PIPE,
-                    shell=True, universal_newlines=True)
-    talk = subproc.communicate()
+    subproc = await asyncSubprocess(cmd, stdout=asyncPIPE, stderr=asyncPIPE)
+    result = await subproc.communicate()
     exitCode = subproc.returncode
     if exitCode != 0:
-        reply += ('```An error was detected while running the subprocess:\n'
-                  f'exit code: {exitCode}\n'
-                  f'stdout: {talk[0]}\n'
-                  f'stderr: {talk[1]}```')
+        reply += (
+            '**An error was detected while running subprocess.**\n'
+            f'exitCode : `{exitCode}`\n'
+            f'stdout : `{result[0].decode().strip()}`\n'
+            f'stderr : `{result[1].decode().strip()}`')
         return reply
-    return talk
+    return result
 
 
 @register(outgoing=True, pattern=r"^.direct(?: |$)([\s\S]*)")
@@ -52,74 +57,32 @@ async def direct_link_generator(request):
         reply = "`No links found!`"
         await request.edit(reply)
     for link in links:
-        if 'drive.google.com' in link:
-            reply += gdrive(link)
-        elif 'zippyshare.com' in link:
-            reply += zippy_share(link)
+        if 'zippyshare.com' in link:
+            reply += await zippy_share(link)
         elif 'yadi.sk' in link:
-            reply += yandex_disk(link)
+            reply += await yandex_disk(link)
         elif 'cloud.mail.ru' in link:
-            reply += cm_ru(link)
+            reply += await cm_ru(link)
         elif 'mediafire.com' in link:
-            reply += mediafire(link)
+            reply += await mediafire(link)
         elif 'sourceforge.net' in link:
-            reply += sourceforge(link)
+            reply += await sourceforge(link)
         elif 'osdn.net' in link:
-            reply += osdn(link)
+            reply += await osdn(link)
         elif 'github.com' in link:
-            reply += github(link)
+            reply += await github(link)
         elif 'androidfilehost.com' in link:
-            reply += androidfilehost(link)
+            reply += await androidfilehost(link)
+        elif 'uptobox.com' in link:
+            await uptobox(request, link)
+            return None
         else:
             reply += re.findall(r"\bhttps?://(.*?[^/]+)",
                                 link)[0] + 'is not supported'
     await request.edit(reply)
 
 
-def gdrive(url: str) -> str:
-    """ GDrive direct links generator """
-    drive = 'https://drive.google.com'
-    try:
-        link = re.findall(r'\bhttps?://drive\.google\.com\S+', url)[0]
-    except IndexError:
-        reply = "`No Google drive links found`\n"
-        return reply
-    file_id = ''
-    reply = ''
-    if link.find("view") != -1:
-        file_id = link.split('/')[-2]
-    elif link.find("open?id=") != -1:
-        file_id = link.split("open?id=")[1].strip()
-    elif link.find("uc?id=") != -1:
-        file_id = link.split("uc?id=")[1].strip()
-    url = f'{drive}/uc?export=download&id={file_id}'
-    download = requests.get(url, stream=True, allow_redirects=False)
-    cookies = download.cookies
-    try:
-        # In case of small file size, Google downloads directly
-        dl_url = download.headers["location"]
-        if 'accounts.google.com' in dl_url:  # non-public file
-            reply += '`Link is not public!`\n'
-            return reply
-        name = 'Direct Download Link'
-    except KeyError:
-        # In case of download warning page
-        page = BeautifulSoup(download.content, 'lxml')
-        export = drive + page.find('a', {'id': 'uc-download-link'}).get('href')
-        name = page.find('span', {'class': 'uc-name-size'}).text
-        response = requests.get(export,
-                                stream=True,
-                                allow_redirects=False,
-                                cookies=cookies)
-        dl_url = response.headers['location']
-        if 'accounts.google.com' in dl_url:
-            reply += 'Link is not public!'
-            return reply
-    reply += f'[{name}]({dl_url})\n'
-    return reply
-
-
-def zippy_share(url: str) -> str:
+async def zippy_share(url: str) -> str:
     """ ZippyShare direct links generator
     Based on https://github.com/LameLemon/ziggy"""
     reply = ''
@@ -148,7 +111,7 @@ def zippy_share(url: str) -> str:
     return reply
 
 
-def yandex_disk(url: str) -> str:
+async def yandex_disk(url: str) -> str:
     """ Yandex.Disk direct links generator
     Based on https://github.com/wldhx/yadisk-direct"""
     reply = ''
@@ -168,7 +131,7 @@ def yandex_disk(url: str) -> str:
     return reply
 
 
-def cm_ru(url: str) -> str:
+async def cm_ru(url: str) -> str:
     """ cloud.mail.ru direct links generator
     Using https://github.com/JrMasterModelBuilder/cmrudl.py"""
     reply = ''
@@ -194,7 +157,7 @@ def cm_ru(url: str) -> str:
     return reply
 
 
-def mediafire(url: str) -> str:
+async def mediafire(url: str) -> str:
     """ MediaFire direct links generator """
     try:
         link = re.findall(r'\bhttps?://.*mediafire\.com\S+', url)[0]
@@ -211,7 +174,7 @@ def mediafire(url: str) -> str:
     return reply
 
 
-def sourceforge(url: str) -> str:
+async def sourceforge(url: str) -> str:
     """ SourceForge direct links generator """
     try:
         link = re.findall(r'\bhttps?://.*sourceforge\.net\S+', url)[0]
@@ -232,7 +195,7 @@ def sourceforge(url: str) -> str:
     return reply
 
 
-def osdn(url: str) -> str:
+async def osdn(url: str) -> str:
     """ OSDN direct links generator """
     osdn_link = 'https://osdn.net'
     try:
@@ -254,7 +217,7 @@ def osdn(url: str) -> str:
     return reply
 
 
-def github(url: str) -> str:
+async def github(url: str) -> str:
     """ GitHub direct links generator """
     try:
         link = re.findall(r'\bhttps?://.*github\.com.*releases\S+', url)[0]
@@ -273,7 +236,7 @@ def github(url: str) -> str:
     return reply
 
 
-def androidfilehost(url: str) -> str:
+async def androidfilehost(url: str) -> str:
     """ AFH direct links generator """
     try:
         link = re.findall(r'\bhttps?://.*androidfilehost.*fid.*\S+', url)[0]
@@ -282,7 +245,7 @@ def androidfilehost(url: str) -> str:
         return reply
     fid = re.findall(r'\?fid=(.*)', link)[0]
     session = requests.Session()
-    user_agent = useragent()
+    user_agent = await useragent()
     headers = {'user-agent': user_agent}
     res = session.get(link, headers=headers, allow_redirects=True)
     headers = {
@@ -324,7 +287,88 @@ def androidfilehost(url: str) -> str:
     return reply
 
 
-def useragent():
+async def uptobox(request, url: str) -> str:
+    """ Uptobox direct links generator """
+    try:
+        link = re.findall(r'\bhttps?://.*uptobox\.com\S+', url)[0]
+    except IndexError:
+        await request.edit('`No uptobox links found.`')
+        return
+    if USR_TOKEN is None:
+        await request.edit('`Set USR_TOKEN_UPTOBOX first!`')
+        return
+    if link.endswith('/'):
+        index = -2
+    else:
+        index = -1
+    FILE_CODE = link.split('/')[index]
+    origin = 'https://uptobox.com/api/link'
+    """ Retrieve file informations """
+    uri = f'{origin}/info?fileCodes={FILE_CODE}'
+    await request.edit('`Retrieving file informations...`')
+    async with aiohttp.ClientSession() as session:
+        async with session.get(uri) as response:
+            result = json.loads(await response.text())
+            data = result.get('data').get('list')[0]
+            if 'error' in data:
+                await request.edit(
+                    "`[ERROR]`\n"
+                    f"`statusCode`: **{data.get('error').get('code')}**\n"
+                    f"`reason`: **{data.get('error').get('message')}**"
+                )
+                return
+            file_name = data.get('file_name')
+            file_size = naturalsize(data.get('file_size'))
+    """ Get waiting token and direct download link """
+    uri = f'{origin}?token={USR_TOKEN}&file_code={FILE_CODE}'
+    async with aiohttp.ClientSession() as session:
+        async with session.get(uri) as response:
+            result = json.loads(await response.text())
+            status = result.get('message')
+            if status == "Waiting needed":
+                wait = result.get('data').get('waiting')
+                waitingToken = result.get('data').get('waitingToken')
+                await request.edit(
+                    f'`Waiting for about {time_formatter(wait)}.`')
+                # for some reason it doesn't go as i planned
+                # so make it 1 minute just to be save enough
+                await asyncio.sleep(wait + 60)
+                uri += f"&waitingToken={waitingToken}"
+                async with session.get(uri) as response:
+                    await request.edit('`Generating direct download link...`')
+                    result = json.loads(await response.text())
+                    status = result.get('message')
+                    if status == "Success":
+                        webLink = result.get('data').get('dlLink')
+                        await request.edit(
+                            f"[{file_name} ({file_size})]({webLink})"
+                        )
+                        return
+                    else:
+                        await request.edit(
+                            "`[ERROR]`\n"
+                            f"`statusCode`: **{result.get('statusCode')}**\n"
+                            f"`reason`: **{result.get('data')}**\n"
+                            f"`status`: **{status}**"
+                        )
+                        return
+            elif status == "Success":
+                webLink = result.get('data').get('dlLink')
+                await request.edit(
+                    f"[{file_name} ({file_size})]({webLink})"
+                )
+                return
+            else:
+                await request.edit(
+                    "`[ERROR]`\n"
+                    f"`statusCode`: **{result.get('statusCode')}**\n"
+                    f"`reason`: **{result.get('data')}**\n"
+                    f"`status`: **{status}**"
+                )
+                return
+
+
+async def useragent():
     """
     useragent random setter
     """
@@ -339,8 +383,8 @@ def useragent():
 
 CMD_HELP.update({
     "direct":
-    ".direct <url>\n"
-    "Usage: Reply to a link or paste a URL to\n"
+    ".direct <url>"
+    "\nUsage: Reply to a link or paste a URL to\n"
     "generate a direct download link\n\n"
     "List of supported URLs:\n"
     "`Google Drive - Cloud Mail - Yandex.Disk - AFH - "
